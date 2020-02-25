eca28d6adb627b75974febb6bb0f8635d7d28a76
dpancho <dalepancho7@gmail.com>
2020-02-25 09:37:27 -0800

    trying to fix import problem

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,28 @@
+from doubly_linked_list import DoublyLinkedList
 import sys
 sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
 
 
 class Queue:
     def __init__(self):
         self.size = 0
+        self.list = DoublyLinkedList()
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        return self.storage.add_to_head(value)
+
+    def is_empty(self):
+        return self.size == 0
 
     def dequeue(self):
-        pass
+        if self.is_empty():
+            return
+        else:
+            self.size -= 1
+            return self.storage.remove_from_tail(value)
 
     def len(self):
-        pass
+        return self.size
